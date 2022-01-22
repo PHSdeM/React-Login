8bf32b71881a99fa8a8e70843683b052a24079fb
phsdem <pedrohsantosdemoura@gmail.com>
2022-01-22 12:52:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import logoRum from './assets/img/rum.svg'
+import './assets/css/styles.css'
+
 
 function App() {
+  const [email, setEmail ] = useState("")
+  const [password, setPassword ] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <div className="container-login">
+       <div className="wrap-login">
+        <form className="login-form">
+        
+        <span className="login-form-title">Bem Vindo!</span>
+       
+        <span className="login-form-title">
+          <img src={logoRum} alt="Rum Logo"/>
+        </span>
+
+        <div className='wrap-input'>
+          <input
+          className={email !== "" ? 'has-val input' : 'input' }
+          type="email" 
+          value={email}
+          onChange={e => setEmail(e.target.value)}/>
+          <span className='focus-input' data-placeholder="Email"></span>
+        </div>
+
+        <div className='wrap-input'>
+          <input 
+          className={password !== "" ? 'has-val input' : 'input' }
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}/>
+          <span className='focus-input' data-placeholder="Password"></span>
+        </div>
+
+        <div className='container-form-btn'>
+          <button className='login-form-btn'>Login</button>
+        </div>
+
+        <div className='text-center'>
+          <span className='text'>Não possui conta?</span>
+          <a className='text-a' href='#a'>Criar conta.</a>
+        </div>
+        </form>
+       </div>
+     </div>
+   </div>
   );
 }
 
